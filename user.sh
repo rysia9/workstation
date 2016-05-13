b63f8a940e54fd5f75deef6e251600f10f5a1557
vakhnenko <dmitriy@live.ru>
2016-05-13 14:41:20 +0300

    Update user.sh

--- user.sh
+++ user.sh
@@ -15,19 +15,20 @@ rm -rf $HOME/.zgen
 git clone https://github.com/tarjoilija/zgen.git $HOME/.zgen
 
 # Atom packages
-apm i todo-show highlight-selected color-picker pigments linter linter-rubocop \
-      linter-coffeelint linter-jshint linter-pylint atom-beautify atom-alignment \
-      file-icons git-plus browser-plus script markdown-preview-plus terminal-panel \
-      compare-files emmet project-manager
+#apm i todo-show highlight-selected color-picker pigments linter linter-rubocop \
+#      linter-coffeelint linter-jshint linter-pylint atom-beautify atom-alignment \
+#      file-icons git-plus browser-plus script markdown-preview-plus terminal-panel \
+#      compare-files emmet project-manager
 
 # Icon theme
 rm -rf $HOME/Downloads/paper-icon
+rm -rf $HOME/.local/share/icons/Paper
 git clone https://github.com/snwh/paper-icon-theme.git $HOME/Downloads/paper-icon
 mv --force $HOME/Downloads/paper-icon/Paper $HOME/.local/share/icons/Paper
 
 # Configure
 cp --force files/zshrc $HOME/.zshrc # zsh
 #cp --force files/Gemfile $HOME/work/workstation/files/Gemfile
-rbenv install 2.2.5
-rbenv global 2.2.5
-gem install bundler
+#rbenv install 2.2.5
+#rbenv global 2.2.5
+#gem install bundler
