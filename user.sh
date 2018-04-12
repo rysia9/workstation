2dc416518e853f161c4d42422a60c636326f4922
Ruslan Tovmasyan <sanslar.com@gmail.com>
2018-04-12 18:58:41 +0300

    Update user.sh

--- user.sh
+++ user.sh
@@ -1,18 +1,21 @@
 #!/bin/sh -exu
 
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+cp --force files/zshrc $HOME/.zshrc # zsh
+
 # Ruby
-rm -rf $HOME/.rbenv
-git clone https://github.com/rbenv/rbenv.git $HOME/.rbenv
-git clone https://github.com/rbenv/ruby-build.git $HOME/.rbenv/plugins/ruby-build
-git clone git://github.com/jf/rbenv-gemset.git $HOME/.rbenv/plugins/rbenv-gemset
+#rm -rf $HOME/.rbenv
+#git clone https://github.com/rbenv/rbenv.git $HOME/.rbenv
+#git clone https://github.com/rbenv/ruby-build.git $HOME/.rbenv/plugins/ruby-build
+#git clone git://github.com/jf/rbenv-gemset.git $HOME/.rbenv/plugins/rbenv-gemset
 
 # NodeJS
-rm -rf $HOME/.nvm
-curl -o- https://raw.githubusercontent.com/creationix/nvm/master/install.sh | zsh
+#rm -rf $HOME/.nvm
+#curl -o- https://raw.githubusercontent.com/creationix/nvm/master/install.sh | zsh
 
 # Zgen
-rm -rf $HOME/.zgen
-git clone https://github.com/tarjoilija/zgen.git $HOME/.zgen
+#rm -rf $HOME/.zgen
+#git clone https://github.com/tarjoilija/zgen.git $HOME/.zgen
 
 # Atom packages
 #apm i todo-show highlight-selected color-picker pigments linter linter-rubocop \
@@ -21,13 +24,12 @@ git clone https://github.com/tarjoilija/zgen.git $HOME/.zgen
 #      compare-files emmet project-manager
 
 # Icon theme
-rm -rf $HOME/Downloads/paper-icon
-rm -rf $HOME/.local/share/icons/Paper
-git clone https://github.com/snwh/paper-icon-theme.git $HOME/Downloads/paper-icon
-mv --force $HOME/Downloads/paper-icon/Paper $HOME/.local/share/icons/Paper
+#rm -rf $HOME/Downloads/paper-icon
+#rm -rf $HOME/.local/share/icons/Paper
+#git clone https://github.com/snwh/paper-icon-theme.git $HOME/Downloads/paper-icon
+#mv --force $HOME/Downloads/paper-icon/Paper $HOME/.local/share/icons/Paper
 
 # Configure
-cp --force files/zshrc $HOME/.zshrc # zsh
 #cp --force files/Gemfile $HOME/work/workstation/files/Gemfile
 #rbenv install 2.2.5
 #rbenv global 2.2.5
